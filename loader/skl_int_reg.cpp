2085a0dfba95d24a41156afdbb99f0f4c0ab1a1b
thutt <thutt@harp-project.com>
2022-11-30 19:31:13 -0800

    [loader] Create classes for integer register instructions
    Details
    
      This change updates the module that processes integer register
      instructions to have a class for each instruction.
    
      This removes an indirect call used to implement all instructions
      with a single class.  The replacement is inline code in each
      instruction class.
    
    Testing
    
      With an alpha type build:
    
        skl-build -j 10
        ./scripts/skl-compile-os-skl
        ./scripts/skl-test-system
        ./scripts/skl-test-compiler
        ./scripts/skl-test-modules
    
      With a release type build:
    
        skl-build -j 10
        ./scripts/skl-compile-os-skl
        ./scripts/skl-test-system
        ./scripts/skl-test-compiler
        ./scripts/skl-test-modules
    
      ./scripts/skl-size --compare /tmp/skl/github/release
    
                                      .text         .rodata           .data            .bss
      release/loader/oberon     37K    (1K)      9K   (400)      1K   (-96)      3K     (0)
      release/disasm/disasm     24K     (0)      7K     (0)      64     (0)     280     (0)

--- loader/skl_int_reg.cpp
+++ loader/skl_int_reg.cpp
@@ -22,243 +22,329 @@ namespace skl {
 #undef OPC
     };
 
-    typedef md::uint32 (*int_opc_fn_t)(md::uint32 l,
-                                       md::uint32 r,
-                                       bool &fault);
 
-#define OPC(_t) static md::uint32 oper_##_t(md::uint32 l,       \
-                                            md::uint32 r,       \
-                                            bool &fault);
-#include "skl_int_reg_opc.h"
-#undef OPC
-
-    int_opc_fn_t operation[N_OPCODES] = {
-#define OPC(_t) oper_##_t,
-#include "skl_int_reg_opc.h"
-#undef OPC
-    };
-
-
-    // XXX TODO: special ize subclasses to remove indirect call of operand.
-    struct skl_int_reg_t : skl::instruction_t {
+    struct skl_int_reg_base_t : skl::instruction_t {
         int          Rd;
         int          R0;
         int          R1;
-        int_opc_fn_t operation;
 
-        skl_int_reg_t(md::OADDR    pc_,
+        skl_int_reg_base_t(md::OADDR    pc_,
                       md::OINST   inst_,
-                      const char **mne_,
-                      int_opc_fn_t operation_) :
+                      const char **mne_) :
             skl::instruction_t(pc_, inst_, mne_),
             Rd(field(inst_, 25, 21)),
             R0(field(inst_, 20, 16)),
-            R1(field(inst_, 15, 11)),
-            operation(operation_)
+            R1(field(inst_, 15, 11))
+        {
+        }
+    };
+
+
+    struct skl_int_reg_and_t : skl::skl_int_reg_base_t {
+        skl_int_reg_and_t(md::OADDR    pc_,
+                          md::OINST   inst_,
+                          const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
         {
         }
 
 
         virtual void interpret(skl::cpu_t *cpu)
         {
-            md::uint32       v;
-            bool             fault = false;
             const md::uint32 l     = read_integer_register(cpu, R0);
             const md::uint32 r     = read_integer_register(cpu, R1);
+            md::uint32       v     = l & r;
 
             dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
 
-            v = operation(l, r, fault);
             write_integer_register(cpu, Rd, v);
-            if (LIKELY(!fault)) {
+            increment_pc(cpu, 1);
+        }
+    };
+
+
+    struct skl_int_reg_ash_t : skl::skl_int_reg_base_t {
+        skl_int_reg_ash_t(md::OADDR    pc_,
+                          md::OINST   inst_,
+                          const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l     = read_integer_register(cpu, R0);
+            const md::uint32 r     = read_integer_register(cpu, R1);
+            md::uint32       v;
+
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
+
+            if (static_cast<md::int8>(r) >= 0) {
+                v = left_shift(l, static_cast<int>(r));
+                dialog::trace("[%xH, %xH, %xH left]\n", l, r, v);
+            } else {
+                /* Arithmetic shift; do not use right_shift(). */
+                v = static_cast<unsigned>(static_cast<md::int32>(l) >>
+                                          (-static_cast<md::int8>(r) & 31));
+                dialog::trace("[%xH, %xH, %xH right]\n", l, r, v);
+            }
+            write_integer_register(cpu, Rd, v);
+            increment_pc(cpu, 1);
+        }
+    };
+
+
+    struct skl_int_reg_cmps_t : skl::skl_int_reg_base_t {
+        skl_int_reg_cmps_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l = read_integer_register(cpu, R0);
+            const md::uint32 r = read_integer_register(cpu, R1);
+            md::uint32       v;
+            md::HADDR        lp;
+            md::HADDR        rp;
+            int              llen;
+            int              rlen;
+            bool             zf; // Zero flag.
+            bool             cf; // Carry flag.
+            bool             of; // Overflow flag.
+            bool             sf; // Sign flag.
+            bool             valid;
+
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
+
+            lp   = heap::heap_to_host(l);
+            llen = static_cast<int>(strlen(reinterpret_cast<const char *>(lp)));
+            rp   = heap::heap_to_host(r);
+            rlen = static_cast<int>(strlen(reinterpret_cast<const char *>(rp)));
+            valid = (address_valid(l, static_cast<int>(sizeof(md::uint8))) &&
+                     address_valid(l + static_cast<md::OADDR>(llen),
+                                   static_cast<int>(sizeof(md::uint8))) &&
+                     address_valid(r, static_cast<int>(sizeof(md::uint8))) &&
+                     address_valid(r + static_cast<md::OADDR>(rlen),
+                                   static_cast<int>(sizeof(md::uint8))));
+
+            if (LIKELY(valid)) {
+                int compare = strcmp(reinterpret_cast<const char *>(lp),
+                                     reinterpret_cast<const char *>(rp));
+                if (compare == 0) {
+                    zf = true;
+                    cf = false;
+                    of = false;
+                    sf = false;
+                } else if (compare > 0) {
+                    zf = false;
+                    cf = false;
+                    of = false;
+                    sf = false;
+                } else {
+                    /* result < 0 */
+                    zf = false;
+                    cf = false;
+                    of = false;
+                    sf = true;
+                }
+                v = create_flags(zf, sf, cf, of);
+                dialog::trace("[%xH, %xH, %xH]\n", l, r, v);
+                write_integer_register(cpu, Rd, v);
                 increment_pc(cpu, 1);
+            } else {
+                hardware_trap(cpu, CR2_OUT_OF_BOUNDS_READ);
             }
         }
     };
 
 
-    static md::uint32
-    oper_AND(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        md::uint32 v = l & r;
-        dialog::trace("[%xH, %xH, %xH]\n", l, r, v);
-        return v;
-    }
-
-
-    static md::uint32
-    oper_ASH(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        unsigned v;
-
-        if (static_cast<md::int8>(r) >= 0) {
-            v = left_shift(l, static_cast<int>(r));
-            dialog::trace("[%xH, %xH, %xH left]\n", l, r, v);
-        } else {
-            /* Arithmetic shift; do not use right_shift(). */
-            v = static_cast<unsigned>(static_cast<md::int32>(l) >>
-                                      (-static_cast<md::int8>(r) & 31));
-            dialog::trace("[%xH, %xH, %xH right]\n", l, r, v);
+    struct skl_int_reg_bitset_t : skl::skl_int_reg_base_t {
+        skl_int_reg_bitset_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
         }
-        return v;
-    }
 
 
-    static md::uint32
-    oper_CMPS(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        md::uint32 v;
-        md::HADDR  lp;
-        md::HADDR  rp;
-        int        llen;
-        int        rlen;
-        bool       zf;          // Zero flag.
-        bool       cf;          // Carry flag.
-        bool       of;          // Overflow flag.
-        bool       sf;          // Sign flag.
-        bool       valid;
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 l = read_integer_register(cpu, R0);
+            md::uint32 r = read_integer_register(cpu, R1);
+            md::uint32 v;
 
-        lp   = heap::heap_to_host(l);
-        llen = static_cast<int>(strlen(reinterpret_cast<const char *>(lp)));
-        rp   = heap::heap_to_host(r);
-        rlen = static_cast<int>(strlen(reinterpret_cast<const char *>(rp)));
-        valid = (address_valid(l,
-                               static_cast<int>(sizeof(md::uint8))) &&
-                 address_valid(l + static_cast<md::OADDR>(llen),
-                               static_cast<int>(sizeof(md::uint8))) &&
-                 address_valid(r,
-                               static_cast<int>(sizeof(md::uint8))) &&
-                 address_valid(r + static_cast<md::OADDR>(rlen),
-                               static_cast<int>(sizeof(md::uint8))));
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
 
-        if (LIKELY(valid)) {
-            int compare = strcmp(reinterpret_cast<const char *>(lp),
-                                 reinterpret_cast<const char *>(rp));
-            if (compare == 0) {
-                zf = true;
-                cf = false;
-                of = false;
-                sf = false;
-            } else if (compare > 0) {
-                zf = false;
-                cf = false;
-                of = false;
-                sf = false;
+            // md::MaxSet must be one less than a power-of-2.
+            COMPILE_TIME_ASSERT(((md::MaxSet + 1) & md::MaxSet) == 0);
+
+            l &= md::MaxSet;        // Constrain to md::MaxSet
+            r &= md::MaxSet;        // Constrain to md::MaxSet
+            COMPILE_TIME_ASSERT(md::MaxSet <= 31);
+            if (LIKELY(l <= r)) {
+                /* The following expression can overflow md::uint32.  Compute
+                 * as 64-bit value and then coerce to 32-bit value.
+                 *
+                 * Do not use left_shift() due to potential overflow.
+                 */
+                COMPILE_TIME_ASSERT(sizeof(1)   == sizeof(md::uint32) &&
+                                    sizeof(1UL) == sizeof(md::uint64));
+                v = static_cast<md::uint32>(((1UL << (r - l + 1UL)) - 1UL) << l);
+                dialog::trace("[%xH, %xH]    value: %xH]\n", l, r, v);
+                write_integer_register(cpu, Rd, v);
+                increment_pc(cpu, 1);
             } else {
-                /* result < 0 */
-                zf = false;
-                cf = false;
-                of = false;
-                sf = true;
+                software_trap(cpu, 12);
             }
-            v = create_flags(zf, sf, cf, of);
-        } else {
-            v     = create_flags(0, 0, 0, 0);
-            fault = true;
-            hardware_trap(&cpu, CR2_OUT_OF_BOUNDS_READ);
         }
-        dialog::trace("[%xH, %xH, %xH]\n", l, r, v);
-        return v;
-    }
+    };
 
 
-    static md::uint32
-    oper_BITSET(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        md::uint32 v;
+    struct skl_int_reg_lsh_t : skl::skl_int_reg_base_t {
+        skl_int_reg_lsh_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
+        }
 
-        // md::MaxSet must be one less than a power-of-2.
-        COMPILE_TIME_ASSERT(((md::MaxSet + 1) & md::MaxSet) == 0);
 
-        l &= md::MaxSet;        // Constrain to md::MaxSet
-        r &= md::MaxSet;        // Constrain to md::MaxSet
-        COMPILE_TIME_ASSERT(md::MaxSet <= 31);
-        if (LIKELY(l <= r)) {
-            /* The following expression can overflow md::uint32.  Compute
-             * as 64-bit value and then coerce to 32-bit value.
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l     = read_integer_register(cpu, R0);
+            const md::uint32 r     = read_integer_register(cpu, R1);
+            md::uint32       v;
+
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
+            if (static_cast<md::int8>(r) >= 0) {
+                v = left_shift(l, static_cast<int>(r));
+                dialog::trace("[%xH, %xH, %xH left]\n", l, r, v);
+            } else {
+                v = right_shift(l, -static_cast<md::int8>(r));
+                dialog::trace("[%xH, %xH, %xH right]\n", l, r, v);
+            }
+
+            write_integer_register(cpu, Rd, v);
+            increment_pc(cpu, 1);
+        }
+    };
+
+
+    struct skl_int_reg_nor_t : skl::skl_int_reg_base_t {
+        skl_int_reg_nor_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l = read_integer_register(cpu, R0);
+            const md::uint32 r = read_integer_register(cpu, R1);
+            md::uint32       v = ~(l | r);
+
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
+
+            write_integer_register(cpu, Rd, v);
+            increment_pc(cpu, 1);
+        }
+    };
+
+
+    struct skl_int_reg_or_t : skl::skl_int_reg_base_t {
+        skl_int_reg_or_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l = read_integer_register(cpu, R0);
+            const md::uint32 r = read_integer_register(cpu, R1);
+            md::uint32       v = l | r;
+
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
+
+            write_integer_register(cpu, Rd, v);
+            increment_pc(cpu, 1);
+        }
+    };
+
+
+    struct skl_int_reg_rot_t : skl::skl_int_reg_base_t {
+        skl_int_reg_rot_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l           = read_integer_register(cpu, R0);
+            const md::uint32 r           = read_integer_register(cpu, R1);
+            int              n_bits      = 32;
+            bool             rotate_left = static_cast<md::int32>(r) >= 0;
+            int              bits        = abs(static_cast<int>(r)) % n_bits;
+            unsigned         lbits;
+            unsigned         rbits;
+            md::uint32       v;
+
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
+
+            /* Rotate l by r.  If r is positive, rotate left.  If r is
+             * negative, rotate right.
              *
-             * Do not use left_shift() due to potential overflow.
+             * Shifting is limited to 31 bits.
              */
-            COMPILE_TIME_ASSERT(sizeof(1)   == sizeof(md::uint32) &&
-                                sizeof(1UL) == sizeof(md::uint64));
-            v = static_cast<md::uint32>(((1UL << (r - l + 1UL)) - 1UL) << l);
-        } else {
-            v     = ~0U;
-            fault = true;
-            software_trap(&cpu, 12);
+            if (rotate_left) {
+                lbits = left_shift(l, bits);
+                rbits = right_shift(l, n_bits - bits);
+            } else {
+                lbits = right_shift(l, bits);
+                rbits = left_shift(l, n_bits - bits);
+            }
+            dialog::trace("[%xH | %xH -> %xH]\n",
+                          lbits, rbits, lbits | rbits);
+            v = lbits | rbits;
+            write_integer_register(cpu, Rd, v);
+            increment_pc(cpu, 1);
         }
-        dialog::trace("[%xH, %xH]    value: %xH]\n", l, r, v);
-        return v;
-    }
+    };
 
 
-    static md::uint32
-    oper_LSH(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        unsigned v;
-
-        if (static_cast<md::int8>(r) >= 0) {
-            v = left_shift(l, static_cast<int>(r));
-            dialog::trace("[%xH, %xH, %xH left]\n", l, r, v);
-        } else {
-            v = right_shift(l, -static_cast<md::int8>(r));
-            dialog::trace("[%xH, %xH, %xH right]\n", l, r, v);
+    struct skl_int_reg_xor_t : skl::skl_int_reg_base_t {
+        skl_int_reg_xor_t(md::OADDR    pc_,
+                           md::OINST   inst_,
+                           const char **mne_) :
+            skl_int_reg_base_t(pc_, inst_, mne_)
+        {
         }
-        return v;
-    }
 
 
-    static md::uint32
-    oper_NOR(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        md::uint32 v = ~(l | r);
-        dialog::trace("[%xH, %xH, %xH]\n", l, r, v);
-        return v;
-    }
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            const md::uint32 l = read_integer_register(cpu, R0);
+            const md::uint32 r = read_integer_register(cpu, R1);
+            md::uint32       v = l ^ r;
 
+            dialog::trace("%s: %s  R%u, R%u, R%u", decoded_pc, mne, R0, R1, Rd);
 
-    static md::uint32
-    oper_OR(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        md::uint32 v = l | r;
-        dialog::trace("[%xH, %xH, %xH]\n", l, r, v);
-        return v;
-    }
-
-
-    static md::uint32
-    oper_ROT(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        /* Rotate l by r.  If r is positive, rotate left.  If r is
-         * negative, rotate right.
-         *
-         * Shifting is limited to 31 bits.
-         */
-        int      n_bits      = 32;
-        bool     rotate_left = static_cast<md::int32>(r) >= 0;
-        int      bits        = abs(static_cast<int>(r)) % n_bits;
-        unsigned lbits;
-        unsigned rbits;
-        if (rotate_left) {
-            lbits = left_shift(l, bits);
-            rbits = right_shift(l, n_bits - bits);
-        } else {
-            lbits = right_shift(l, bits);
-            rbits = left_shift(l, n_bits - bits);
+            write_integer_register(cpu, Rd, v);
+            increment_pc(cpu, 1);
         }
-        dialog::trace("[%xH | %xH -> %xH]\n",
-                      lbits, rbits, lbits | rbits);
-        return lbits | rbits;
-    }
-
-
-    static md::uint32
-    oper_XOR(md::uint32 l, md::uint32 r, bool &fault)
-    {
-        md::uint32 v = l ^ r;
-        dialog::trace("[%xH, %xH, %xH]\n", l, r, v);
-        return v;
-    }
+    };
 
 
     skl::instruction_t *
@@ -267,6 +353,36 @@ namespace skl {
         const opc_t opc = static_cast<opc_t>(field(inst, 4, 0));
 
         assert(opc >= 0 && opc < N_OPCODES);
-        return new skl_int_reg_t(cpu->pc, inst, mne, operation[opc]);
+        switch (opc) {
+        case OPC_AND:
+            return new skl_int_reg_and_t(cpu->pc, inst, mne);
+
+        case OPC_ASH:
+            return new skl_int_reg_ash_t(cpu->pc, inst, mne);
+
+        case OPC_BITSET:
+            return new skl_int_reg_bitset_t(cpu->pc, inst, mne);
+
+        case OPC_CMPS:
+            return new skl_int_reg_cmps_t(cpu->pc, inst, mne);
+
+        case OPC_LSH:
+            return new skl_int_reg_lsh_t(cpu->pc, inst, mne);
+
+        case OPC_NOR:
+            return new skl_int_reg_nor_t(cpu->pc, inst, mne);
+
+        case OPC_OR:
+            return new skl_int_reg_or_t(cpu->pc, inst, mne);
+
+        case OPC_ROT:
+            return new skl_int_reg_rot_t(cpu->pc, inst, mne);
+
+        case OPC_XOR:
+            return new skl_int_reg_xor_t(cpu->pc, inst, mne);
+
+        default:
+            dialog::internal_error("%s: improper opcode", __func__);
+        }
     }
 }
