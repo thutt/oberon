86e13211ebc1826ad7965a5a02af46c3f37de6cc
thutt <thutt@harp-project.com>
2022-12-01 09:56:49 -0800

    [loader] Create specialized traprange instruction classes
    Details
    
      The original 'traprange' implementation contained a 'switch'
      statement in the middle of interpret() to select the proper range
      checking to perform.  This meant that every traprange instruction
      interpretation performed additional checks to determine the
      'subclass' of the range at runtime; the subclass of the instruction
      is known at instance creation time.  To remove this unnecessary
      runtime overhead, the traprange instruction has now been subclassed
      into four:
    
        skl_traprange_sint_t
        skl_traprange_int_t
        skl_traprange_lint_t
        skl_traprange_bitset_t
    
       Additionally, the common 'epilog' code has been moved into the
       'epilog' method; the compiler will inline this at each place it is
       used.
    
    Testing
    
      With an alpha type build:
    
        skl-build -j 10
        ./scripts/skl-compile-os-skl
        ./scripts/skl-test-system
        ./scripts/skl-test-compiler
        ./scripts/skl-test-modules
    
      With a release type build:
    
        skl-build -j 10
        ./scripts/skl-compile-os-skl
        ./scripts/skl-test-system
        ./scripts/skl-test-compiler
        ./scripts/skl-test-modules
    
      The performance improvements from this change will be borne by
      longer-running programs with many traprange instructions.
    
      ./scripts/skl-size --compare /tmp/skl/github/release
    
                                      .text         .rodata           .data            .bss
      release/disasm/disasm     24K     (0)      7K     (0)      64     (0)     280     (0)
      release/loader/oberon     38K   (464)      9K    (96)      1K     (0)      3K     (0)

--- loader/skl_systrap.cpp
+++ loader/skl_systrap.cpp
@@ -38,6 +38,16 @@ namespace skl {
             opc = field(inst, 15,  8);
             mne = mne_[opc];
         }
+
+
+        void epilog(skl::cpu_t *cpu, bool trapped)
+        {
+            if (LIKELY(!trapped)) {
+                increment_pc(cpu, 1);
+            } else {
+                software_trap(cpu, opc);
+            }
+        }
     };
 
 
@@ -58,11 +68,7 @@ namespace skl {
             bool trapped = r0 == 0;         // true => trap raised.
 
             dialog::trace("%s: %s  R%u, %u\n", decoded_pc, mne, R0, subcl);
-            if (LIKELY(!trapped)) {
-                increment_pc(cpu, 1);
-            } else {
-                software_trap(cpu, opc);
-            }
+            epilog(cpu, trapped);
         }
     };
 
@@ -75,44 +81,91 @@ namespace skl {
             skl_trap_t(pc_, inst_, mne_)
         {
         }
+    };
+
+
+    struct skl_traprange_sint_t : skl_traprange_t {
+
+        skl_traprange_sint_t(md::OADDR    pc_,
+                             md::OINST    inst_,
+                             const char **mne_) :
+            skl_traprange_t(pc_, inst_, mne_)
+        {
+        }
 
 
         virtual void interpret(skl::cpu_t *cpu)
         {
             md::uint32 r0    = read_integer_register(cpu, R0);
             int        value = static_cast<int>(r0);
-            bool       ok;
-            bool       trapped;
+            bool       ok    = -128 <= value && value <= 127;
 
-            switch (subcl) {
-            case 4:                 // 1 byte signed integer.
-                ok = -128 <= value && value <= 127;
-                break;
-
-            case 5:                 // 2 byte signed integer.
-                ok = -32768 <= value && value <= 32767;
-                break;
-
-            case 6:                 // 4 byte signed integer.
-                ok = true;          // It cannot be out-of-range!
-                break;
-
-            case 9:                 // 32 bit bitset.
-                ok = 0 <= value && value <= 31;
-                break;
-
-            default:                // All others invalid.
-                ok = false;
-                dialog::internal_error("%s: Invalid 'traprange' encoding (subcl: %u)",
-                                       __func__, subcl);
-            }
             dialog::trace("%s: %s  R%u, %u\n", decoded_pc, mne, R0, subcl);
-            trapped = !ok;             // true => trap raised.
-            if (LIKELY(!trapped)) {
-                increment_pc(cpu, 1);
-            } else {
-                software_trap(cpu, opc);
-            }
+            epilog(cpu, !ok);
+        }
+    };
+
+
+    struct skl_traprange_int_t : skl_traprange_t {
+
+        skl_traprange_int_t(md::OADDR    pc_,
+                            md::OINST    inst_,
+                            const char **mne_) :
+            skl_traprange_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 r0    = read_integer_register(cpu, R0);
+            int        value = static_cast<int>(r0);
+            bool       ok    = -32768 <= value && value <= 32767;
+
+            dialog::trace("%s: %s  R%u, %u\n", decoded_pc, mne, R0, subcl);
+            epilog(cpu, !ok);
+        }
+    };
+
+
+    struct skl_traprange_lint_t : skl_traprange_t {
+
+        skl_traprange_lint_t(md::OADDR    pc_,
+                            md::OINST    inst_,
+                            const char **mne_) :
+            skl_traprange_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            /* LONGINT types can never exceed their range. */
+            bool ok = true;
+            dialog::trace("%s: %s  R%u, %u\n", decoded_pc, mne, R0, subcl);
+            epilog(cpu, !ok);
+        }
+    };
+
+
+    struct skl_traprange_bitset_t : skl_traprange_t {
+
+        skl_traprange_bitset_t(md::OADDR    pc_,
+                               md::OINST    inst_,
+                               const char **mne_) :
+            skl_traprange_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 r0    = read_integer_register(cpu, R0);
+            int        value = static_cast<int>(r0);
+            bool       ok    = 0 <= value && value <= 31;
+
+            dialog::trace("%s: %s  R%u, %u\n", decoded_pc, mne, R0, subcl);
+            epilog(cpu, !ok);
         }
     };
 
@@ -132,18 +185,12 @@ namespace skl {
             md::uint32 r0 = read_integer_register(cpu, R0);
             md::uint32 rd = read_integer_register(cpu, Rd);
             bool       ok;
-            bool       trapped;
 
             ok = (static_cast<int>(r0) >= 0 && // Non-negative index
                   r0 <= rd);                   // R0 is index (x[R0]). Rd is LEN(x).
 
             dialog::trace("%s: %s  R%u, R%u\n", decoded_pc, mne, R0, Rd);
-            trapped = !ok;             // true => trap raised.
-            if (LIKELY(!trapped)) {
-                increment_pc(cpu, 1);
-            } else {
-                software_trap(cpu, opc);
-            }
+            epilog(cpu, !ok);
         }
     };
 
@@ -158,8 +205,18 @@ namespace skl {
         case OPC_TRAPNIL:
             return new skl_trapnil_t(cpu->pc, inst, mne);
 
-        case OPC_TRAPRANGE:
-            return new skl_traprange_t(cpu->pc, inst, mne);
+        case OPC_TRAPRANGE: {
+            int subcl = field(inst, 7, 0);
+
+            switch (subcl) {
+            case 4 : return new skl_traprange_sint_t(cpu->pc, inst, mne);
+            case 5 : return new skl_traprange_int_t(cpu->pc, inst, mne);
+            case 6 : return new skl_traprange_lint_t(cpu->pc, inst, mne);
+            case 9 : return new skl_traprange_bitset_t(cpu->pc, inst, mne);
+            default: dialog::internal_error("%s: traprange (subcl: %u)",
+                                            __func__, subcl);
+            }
+        }
 
         case OPC_TRAPARRAY:
             return new skl_traparray_t(cpu->pc, inst, mne);
