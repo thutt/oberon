6c29d52d4e854c1368db471d8358b6d846db70db
thutt <thutt@harp-project.com>
2022-12-01 18:21:37 -0800

    [loader] Fix & improve conditional set family
    Details
    
      A previous change created a set of inline functions to facilitate
      removal of indirect function calls in several classes of
      instructions.  The first one to take advantage of this previous
      change is the conditional set family by making specialized
      skl::instruction_t extensions to handle each instruction in the
      family.  This, however, exposed a defect:
    
        The conditional set instructions have opcodes that match the
        conditional jump instructions' opcodes, but the defined values
        were incorrect.  A rearrangement in the documentation and the
        loader / interpreter addressed the issue.  The compiler and the
        diassembler already had the correct values.
    
      This was not a problem because the original conditional set
      interpreter did not use the opcodes symbolically, as the new code
      does for creating the instruction instances.
    
    Testing
    
      With an alpha type build:
    
        skl-build -j 10
        ./scripts/skl-compile-os-skl
        ./scripts/skl-test-system
        ./scripts/skl-test-compiler
        ./scripts/skl-test-modules
    
      With a release type build:
    
        skl-build -j 10
        ./scripts/skl-compile-os-skl
        ./scripts/skl-test-system
        ./scripts/skl-test-compiler
        ./scripts/skl-test-modules
    
      The performance improvements from this change will be borne by
      longer-running programs with many traprange instructions.
    
      ./scripts/skl-size --compare /tmp/skl/github/release
    
                                        .text         .rodata           .data            .bss
        release/loader/oberon     39K    (1K)      9K   (448)      1K     (0)      3K     (0)
        release/disasm/disasm     24K     (0)      7K     (0)      64     (0)     280     (0)

--- loader/skl_cond.cpp
+++ loader/skl_cond.cpp
@@ -35,11 +35,8 @@ namespace skl {
         }
 
 
-        virtual void interpret(skl::cpu_t *cpu)
+        void epilog(skl::cpu_t *cpu, bool value, md::uint32 R0v)
         {
-            md::uint32 R0v   = read_integer_register(cpu, R0);
-            bool       value = relation[opc](R0v);
-
             write_integer_register(cpu, Rd, value);
             increment_pc(cpu, 1);
             dialog::trace("%xH: %s  R%u, R%u  [%xH, %u]\n",
@@ -48,12 +45,197 @@ namespace skl {
     };
 
 
+    struct skl_seq_t : skl::skl_conditional_set_t {
+        skl_seq_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_eq(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sge_t : skl::skl_conditional_set_t {
+        skl_sge_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_ge(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sgeu_t : skl::skl_conditional_set_t {
+        skl_sgeu_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_geu(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sgt_t : skl::skl_conditional_set_t {
+        skl_sgt_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_gt(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sgtu_t : skl::skl_conditional_set_t {
+        skl_sgtu_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_gtu(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sle_t : skl::skl_conditional_set_t {
+        skl_sle_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_le(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sleu_t : skl::skl_conditional_set_t {
+        skl_sleu_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_leu(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_slt_t : skl::skl_conditional_set_t {
+        skl_slt_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_lt(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sltu_t : skl::skl_conditional_set_t {
+        skl_sltu_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_ltu(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
+    struct skl_sne_t : skl::skl_conditional_set_t {
+        skl_sne_t(md::OADDR pc_, md::OINST inst_, const char **mne_) :
+            skl::skl_conditional_set_t(pc_, inst_, mne_)
+        {
+        }
+
+
+        virtual void interpret(skl::cpu_t *cpu)
+        {
+            md::uint32 R0v   = read_integer_register(cpu, R0);
+            bool       value = relation_ne(R0v);
+
+            epilog(cpu, value, R0v);
+        }
+    };
+
+
     skl::instruction_t *
     op_conditional_set(cpu_t *cpu, md::OINST inst)
     {
+
+        opc_t opc = static_cast<opc_t>(field(inst, 4, 0));
+
         COMPILE_TIME_ASSERT(N_OPCODES == (sizeof(relation) /
                                           sizeof(relation[0])));
 
-        return new skl_conditional_set_t(cpu->pc, inst, mne);
+        switch (opc) {
+        case OPC_SEQ:  return new skl_seq_t(cpu->pc, inst, mne);
+        case OPC_SGE:  return new skl_sge_t(cpu->pc, inst, mne);
+        case OPC_SGEU: return new skl_sgeu_t(cpu->pc, inst, mne);
+        case OPC_SGT:  return new skl_sgt_t(cpu->pc, inst, mne);
+        case OPC_SGTU: return new skl_sgtu_t(cpu->pc, inst, mne);
+        case OPC_SLE:  return new skl_sle_t(cpu->pc, inst, mne);
+        case OPC_SLEU: return new skl_sleu_t(cpu->pc, inst, mne);
+        case OPC_SLT:  return new skl_slt_t(cpu->pc, inst, mne);
+        case OPC_SLTU: return new skl_sltu_t(cpu->pc, inst, mne);
+        case OPC_SNE:  return new skl_sne_t(cpu->pc, inst, mne);
+        default:       dialog::internal_error("%s: improper opcode", __func__);
+        }
     }
 }
