5ea19f7a4d87cb178fa890f820a0a3fcb52c6541
thutt <thutt@harp-project.com>
2022-12-06 11:11:51 -0800

    [loader] Add specialization to BT instructions
    Details
    
      Continuing with the effort to make skl::instruction_t interpret()
      have as few branches as possible, this change specializes the 'bit
      test' family of instructions.  The specialization instantiates a
      class each for { read, clear, set } operations.  Previously, a
      single skl::instruction_t extension was instantiated that handles
      all of { read, clear, set } operations via runtime checks.
    
    Testing
    
       With an alpha type build:
    
         skl-build -j 10
         ./scripts/skl-compile-os-skl
         ./scripts/skl-test-system
         ./scripts/skl-test-compiler
         ./scripts/skl-test-modules
    
       With a release type build:
    
         skl-build -j 10
         ./scripts/skl-compile-os-skl
         ./scripts/skl-test-system
         ./scripts/skl-test-compiler
         ./scripts/skl-test-modules
    
      To measure, the following methodology is used with a modified tree
      and an unmodified tree.
    
        Time run of the benchmark 5 times.
        Discard lowest and highest times.
        Average the remaining values.
        Divide number of instructions of the test by the average time.
    
       Two basic 'benchmarks' are used:
    
         o Compile Texts.Mod
    
           time -p (skl-oberon --instruction-count SKL.Compile system/Texts.Mod)
           Instruction Count: 111253668
    
           base:  1.97  1.97  1.97  1.97  1.99   | 1.970 seconds (53.858 MIPS)
           modi:  1.98  1.99  2.00  1.97  1.97   | 1.980 seconds (53.586 MIPS)
    
         o Run Prime.Execute
    
           To get number of instructions:
    
              time -p (skl-oberon --instruction-count system/Prime.Execute)
              Instruction Count: 27589021
    
           To measure time
    
              time -p (skl-oberon system/Prime.Execute >/dev/null)
    
           base:  0.57  0.56  0.57  0.56  0.57   | 0.567 seconds  (46.404 MIPS)
           modi:  0.58  0.57  0.56  0.57  0.57   | 0.570 seconds  (46.160 MIPS)
    
       ./scripts/skl-size --compare /tmp/skl/github/release
    
                                         .text         .rodata           .data            .bss
         release/loader/oberon     44K   (832)     10K   (160)      1K     (0)      3K     (0)
         release/disasm/disasm     24K     (0)      7K     (0)      64     (0)     280     (0)

--- loader/skl_bit_test.cpp
+++ loader/skl_bit_test.cpp
@@ -18,18 +18,60 @@ namespace skl {
 #undef OPC
     };
 
-    typedef enum bit_test_memory_op_t {
-        bt_read,                // Read bit only.
-        bt_clear,               // Clear after read.
-        bt_set                  // Set after read.
-    } bit_test_memory_op_t;
-
-
     static unsigned
     bit_test(unsigned v, int b)
     {
         unsigned r = right_shift(v, b) & 1;
         dialog::trace("[%u, %xH]  [result: %d]\n", b, v, r);
+        return r;
+    }
+
+
+    static unsigned
+    bit_test_memory_read(skl::cpuid_t         cpuid,
+                         md::OADDR            ea,
+                         int                  b)
+    {
+        const md::uint32 v = skl::read(cpuid, ea, true, sizeof(md::uint32));
+        unsigned         r = right_shift(v, b) & 1;
+
+        dialog::trace("[%u, (ea: %xH, value: %xH)]  [%d]\n", b, ea, v, r);
+        return r;
+    }
+
+
+    static unsigned
+    bit_test_memory_clear(skl::cpuid_t         cpuid,
+                          md::OADDR            ea,
+                          int                  b)
+    {
+        const md::uint32 v    = skl::read(cpuid, ea, true, sizeof(md::uint32));
+        unsigned         r    = right_shift(v, b) & 1;
+        unsigned         mask = ~left_shift(1, b);
+        md::uint32       nv   = (v & static_cast<md::uint32>(mask));
+
+        dialog::trace("[%u, (ea: %xH, value: %xH)]  [%d]\n", b, ea, v, r);
+        if (!skl::exception_raised(cpuid)) {
+            skl::write(cpuid, ea, nv, sizeof(md::uint32));
+        }
+        return r;
+    }
+
+
+    static unsigned
+    bit_test_memory_set(skl::cpuid_t         cpuid,
+                          md::OADDR            ea,
+                          int                  b)
+    {
+        const md::uint32 v    = skl::read(cpuid, ea, true, sizeof(md::uint32));
+        unsigned         r    = right_shift(v, b) & 1;
+        unsigned         mask = left_shift(1, b);
+        md::uint32       nv   = (v | static_cast<md::uint32>(mask));
+
+        dialog::trace("[%u, (ea: %xH, value: %xH)]  [%d]\n", b, ea, v, r);
+        if (!skl::exception_raised(cpuid)) {
+            skl::write(cpuid, ea, nv, sizeof(md::uint32));
+        }
         return r;
     }
 
@@ -50,7 +92,7 @@ namespace skl {
 
         virtual void interpret(skl::cpuid_t cpu)
         {
-            const md::uint32 r0 = read_integer_register(cpu, R0);
+            const md::uint32 r0 = read_integer_register(cpu, R0) & md::MaxSet;
             const md::uint32 r1 = read_integer_register(cpu, R1);
             unsigned         b;
 
@@ -89,66 +131,22 @@ namespace skl {
     };
 
 
-    static unsigned
-    bit_test_memory(skl::cpuid_t         cpuid,
-                    md::OADDR            ea,
-                    int                  b,
-                    bit_test_memory_op_t op)
-    {
-        const md::uint32 v = skl::read(cpuid, ea, true, sizeof(md::uint32));
-        unsigned         r = right_shift(v, b) & 1;
-
-        dialog::trace("[%u, (ea: %xH, value: %xH)]  [%d]\n", b, ea, v, r);
-        if (!skl::exception_raised(cpuid)) {
-            switch (op) {
-            case bt_read:
-                break;          // nop
-
-            case bt_clear: {
-                unsigned   mask  = ~left_shift(1, b);
-                md::uint32 nv = (v & static_cast<md::uint32>(mask));
-                skl::write(cpuid, ea, nv, sizeof(md::uint32));
-                break;
-            }
-
-            case bt_set: {
-                unsigned   mask = left_shift(1, b);
-                md::uint32 nv   = (v | static_cast<md::uint32>(mask));
-                skl::write(cpuid, ea, nv, sizeof(md::uint32));
-                break;
-            }
-            }
-        }
-        return r;
-    }
-
-
-    struct skl_btm_family_t : skl::instruction_t {
+    struct skl_btm_base_t : skl::instruction_t {
         int                  Rd;
         int                  R0;
         int                  R1;
-        bit_test_memory_op_t op;
 
-        skl_btm_family_t(md::OADDR              pc_,
-                         md::OINST              inst_,
-                         bit_test_memory_op_t   op_) :
+        skl_btm_base_t(md::OADDR              pc_,
+                       md::OINST              inst_) :
             skl::instruction_t(pc_, inst_, mnemonics),
             Rd(field(inst, 25, 21)),
             R0(field(inst, 20, 16)),
-            R1(field(inst, 15, 11)),
-            op(op_)
+            R1(field(inst, 15, 11))
         {
         }
 
-
-        virtual void interpret(skl::cpuid_t cpuid)
+        void epilog(skl::cpuid_t cpuid, unsigned b)
         {
-            const md::uint32 bit  = read_integer_register(cpuid, R0) & md::MaxSet;
-            const md::OADDR  addr = read_integer_register(cpuid, R1);
-            unsigned         b;
-
-            dialog::trace("%s: %s  R%u, (R%u), R%u", decoded_pc, mne, R0, R1, Rd);
-            b = bit_test_memory(cpuid, addr, static_cast<int>(bit), op);
             if (!skl::exception_raised(cpuid)) {
                 write_integer_register(cpuid, Rd, b);
                 increment_pc(cpuid, 1);
@@ -157,34 +155,132 @@ namespace skl {
     };
 
 
-    struct skl_btmi_family_t : skl::instruction_t {
-        int                  Rd;
-        int                  C;
-        int                  R1;
-        bit_test_memory_op_t op;
-
-        skl_btmi_family_t(md::OADDR            pc_,
-                          md::OINST            inst_,
-                          bit_test_memory_op_t op_) :
-            skl::instruction_t(pc_, inst_, mnemonics),
-            Rd(field(inst, 25, 21)),
-            C(field(inst, 20, 16)),
-            R1(field(inst, 15, 11)),
-            op(op_)
+    struct skl_btm_read_t : skl_btm_base_t {
+        skl_btm_read_t(md::OADDR pc_, md::OINST inst_) :
+            skl_btm_base_t(pc_, inst_)
         {
         }
 
+        virtual void interpret(skl::cpuid_t cpuid)
+        {
+            const md::uint32 bit  = read_integer_register(cpuid, R0) & md::MaxSet;
+            const md::OADDR  addr = read_integer_register(cpuid, R1);
+            unsigned         b;
+
+            dialog::trace("%s: %s  R%u, (R%u), R%u", decoded_pc, mne, R0, R1, Rd);
+            b = bit_test_memory_read(cpuid, addr, static_cast<int>(bit));
+            epilog(cpuid, b);
+        }
+    };
+
+
+    struct skl_btm_clear_t : skl_btm_base_t {
+        skl_btm_clear_t(md::OADDR pc_, md::OINST inst_) :
+            skl_btm_base_t(pc_, inst_)
+        {
+        }
 
         virtual void interpret(skl::cpuid_t cpuid)
         {
-            const md::OADDR addr = read_integer_register(cpuid, R1);
-            unsigned        b    = bit_test_memory(cpuid, addr, C, op);
+            const md::uint32 bit  = read_integer_register(cpuid, R0) & md::MaxSet;
+            const md::OADDR  addr = read_integer_register(cpuid, R1);
+            unsigned         b;
 
+            dialog::trace("%s: %s  R%u, (R%u), R%u", decoded_pc, mne, R0, R1, Rd);
+            b = bit_test_memory_clear(cpuid, addr, static_cast<int>(bit));
+            epilog(cpuid, b);
+        }
+    };
+
+
+    struct skl_btm_set_t : skl_btm_base_t {
+        skl_btm_set_t(md::OADDR pc_, md::OINST inst_) :
+            skl_btm_base_t(pc_, inst_)
+        {
+        }
+
+        virtual void interpret(skl::cpuid_t cpuid)
+        {
+            const md::uint32 bit  = read_integer_register(cpuid, R0) & md::MaxSet;
+            const md::OADDR  addr = read_integer_register(cpuid, R1);
+            unsigned         b;
+
+            dialog::trace("%s: %s  R%u, (R%u), R%u", decoded_pc, mne, R0, R1, Rd);
+            b = bit_test_memory_set(cpuid, addr, static_cast<int>(bit));
+            epilog(cpuid, b);
+        }
+    };
+
+
+    struct skl_btmi_base_t : skl::instruction_t {
+        int                  Rd;
+        int                  C;
+        int                  R1;
+
+        skl_btmi_base_t(md::OADDR            pc_,
+                        md::OINST            inst_) :
+            skl::instruction_t(pc_, inst_, mnemonics),
+            Rd(field(inst, 25, 21)),
+            C(field(inst, 20, 16)),
+            R1(field(inst, 15, 11))
+        {
+        }
+
+        void epilog(skl::cpuid_t cpuid, unsigned b)
+        {
             if (!skl::exception_raised(cpuid)) {
                 write_integer_register(cpuid, Rd, b);
                 increment_pc(cpuid, 1);
             }
             dialog::trace("%s: %s  %xH, (R%u), R%u\n", decoded_pc, mne, C, R1, Rd);
+        }
+    };
+
+
+    struct skl_btmi_read_t : skl_btmi_base_t {
+        skl_btmi_read_t(md::OADDR pc_, md::OINST inst_) :
+            skl_btmi_base_t(pc_, inst_)
+        {
+        }
+
+        virtual void interpret(skl::cpuid_t cpuid)
+        {
+            const md::OADDR addr = read_integer_register(cpuid, R1);
+            unsigned        b    = bit_test_memory_read(cpuid, addr, C);
+
+            epilog(cpuid, b);
+        }
+    };
+
+
+    struct skl_btmi_clear_t : skl_btmi_base_t {
+        skl_btmi_clear_t(md::OADDR pc_, md::OINST inst_) :
+            skl_btmi_base_t(pc_, inst_)
+        {
+        }
+
+        virtual void interpret(skl::cpuid_t cpuid)
+        {
+            const md::OADDR addr = read_integer_register(cpuid, R1);
+            unsigned        b    = bit_test_memory_clear(cpuid, addr, C);
+
+            epilog(cpuid, b);
+        }
+    };
+
+
+    struct skl_btmi_set_t : skl_btmi_base_t {
+        skl_btmi_set_t(md::OADDR pc_, md::OINST inst_) :
+            skl_btmi_base_t(pc_, inst_)
+        {
+        }
+
+        virtual void interpret(skl::cpuid_t cpuid)
+        {
+            const md::OADDR addr = read_integer_register(cpuid, R1);
+            unsigned        b    = bit_test_memory_set(cpuid, addr, C);
+
+            epilog(cpuid, b);
         }
     };
 
@@ -197,12 +293,12 @@ namespace skl {
         switch (opc) {
         case OPC_BT:    return new skl_bt_t(pc, inst);
         case OPC_BTI:   return new skl_bti_t(pc, inst);
-        case OPC_BTM:   return new skl_btm_family_t(pc, inst, bt_read);
-        case OPC_BTMI:  return new skl_btmi_family_t(pc, inst, bt_read);
-        case OPC_BTMC:  return new skl_btm_family_t(pc, inst, bt_clear);
-        case OPC_BTMCI: return new skl_btmi_family_t(pc, inst, bt_clear);
-        case OPC_BTMS:  return new skl_btm_family_t(pc, inst, bt_set);
-        case OPC_BTMSI: return new skl_btmi_family_t(pc, inst, bt_set);
+        case OPC_BTM:   return new skl_btm_read_t(pc, inst);
+        case OPC_BTMI:  return new skl_btmi_read_t(pc, inst);
+        case OPC_BTMC:  return new skl_btm_clear_t(pc, inst);
+        case OPC_BTMCI: return new skl_btmi_clear_t(pc, inst);
+        case OPC_BTMS:  return new skl_btm_set_t(pc, inst);
+        case OPC_BTMSI: return new skl_btmi_set_t(pc, inst);
 
         default:
             dialog::not_implemented("%s: inst: %xH opcode: %xH",
